679e22ce9d10687a89b1b1697d46dcb3b0775131
barca42 <baraboraivanova42@gmail.com>
2022-04-01 18:51:27 +0100

    first task done

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,9 +1,7 @@
 import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
 import java.util.*;
-import javax.swing.*;
-import java.lang.Number;
+import java.util.List;
+
 
 /**
  * 
@@ -12,7 +10,27 @@ import java.lang.Number;
  */
 public class FilesInOut {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String argument = args[0];
+        if (argument.contains("FormatNames -u")){
+            String files = argument.substring(20);
+            String input = files.split(" ")[0];
+            String output = files.split(" ")[1];
+            System.out.println(output);
+            formatStrings(input,output,"upperCase");
+        }
+        else if (argument.contains("FormatNames")){
+            String files = argument.substring(17);
+            String input = files.split(" ")[0];
+            String output = files.split(" ")[1];
+
+            formatStrings(input,output,"firstUpperCase");
+        }
+        if (argument.contains("Java FormatNamesm")){
+
+        }
+
+
         // Replace this with statements to set the file name (input) and file name (output).
         // Initially it will be easier to hardcode suitable file names.
 
@@ -31,4 +49,49 @@ public class FilesInOut {
 
     } // main
 
+    public static void formatStrings(String input, String output, String formatWay) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        String ls = System.getProperty("line.separator");
+
+        while ((line = reader.readLine()) != null) {
+            //transforms line into First Name and Last name both starting with uppercase + appropriate date format
+            if (formatWay.equals("firstUpperCase")){
+                String first = line.split(" ")[0];
+                first = first.substring(0, 1).toUpperCase() + first.substring(1);
+                String last = line.split(" ")[1];
+                last = last.substring(0, 1).toUpperCase() + last.substring(1);
+                String date = (line.split(" ")[2]);
+                date = date.substring(0,2)+"/"+date.substring(2,4)+"/"+date.substring(4,8);
+                line = first+ " "+ last + " "+ date;
+
+            }
+            if (formatWay.equals("upperCase")){
+                //transforms whole name into upperCase and formats date appropriately
+                String first = line.split(" ")[0];
+                first = first.toUpperCase();
+                String last = line.split(" ")[1];
+                last = last.toUpperCase();
+                String date = (line.split(" ")[2]);
+                date = date.substring(0,2)+"/"+date.substring(2,4)+"/"+date.substring(4,8);
+                line = first+ " "+ last + " "+ date;
+
+            }
+            if (formatWay.equals("middleUpperCase")){
+
+            }
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+
+        // delete the last new line separator
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        reader.close();
+        String content = stringBuilder.toString();
+
+        try (PrintWriter out = new PrintWriter(output)) {
+            out.println(content);
+        }
+    }
 } // FilesInOut
